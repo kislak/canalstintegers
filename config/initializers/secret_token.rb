fc674f1c3b903e269d52571535b2d52b5f860cab
Matthew Rothenberg <mrothenberg@gmail.com>
2012-08-15 19:52:01 -0400

    initial import

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Canalstintegers::Application.config.secret_token = 'bfcaf10276de8a6df8212b391044b4e06509e5bde97854c4bd4dab36aa515f3fb21713b75b1829b870a6b0b955e1e78560cee4288c32ca7fc2f78498e8a22ab6'
