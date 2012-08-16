15955cedd29052701695ed615cb4f21a273d0d1b
Matthew Rothenberg <mrothenberg@gmail.com>
2012-08-15 21:36:15 -0400

    minor presence validations

--- app/models/cs_integer.rb
+++ app/models/cs_integer.rb
@@ -1,6 +1,11 @@
 class CsInteger < ActiveRecord::Base
   attr_accessible :celebrity_name, :celebrity_screen_name, :origin, :origin_url
 
+  validates :celebrity_name, :presence => true
+  validates :celebrity_screen_name, :presence => true
+  validates :origin, :presence => true
+  validates :origin_url, :presence => true
+
   def description
     "A [adjective] integer \##{id} as used by #{celebrity_name} on #{origin}"
   end
