05fd1de0788ce5a2ec9c8e0913f2c3595717c0d7
Matthew Rothenberg <mrothenberg@gmail.com>
2012-08-17 18:58:12 -0400

    dont die if cant read a user on seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,22 +7,26 @@
 #   Mayor.create(name: 'Emanuel', city: cities.first)
 
 File.open('config/twitter_celebs.txt').each do |celeb|
-  Twitter.user_timeline( celeb ).each do |t|
+  begin
+    Twitter.user_timeline( celeb ).each do |t|
 
-    if CsInteger.exists? t.id
-      puts "--- INTEGER #{t.id} (as used by #{t.user.name}) already exists"
-    else
-      puts "+++ seeding INTEGER #{t.id} (as used by #{t.user.name} on Twitter)"
-      url = "http://twitter.com/#{t.user.screen_name}/status/#{t.id}"
-      # note: don't use #create since id is attr_protected
-      csi = CsInteger.new( 
-        :celebrity_name => t.user.name,
-        :celebrity_screen_name => t.user.screen_name,
-        :origin => 'Twitter',
-        :origin_url => url
-      )
-      csi.id = t.id #likewise, need to set id seperately here
-      csi.save!
+      if CsInteger.exists? t.id
+        puts "--- INTEGER #{t.id} (as used by #{t.user.name}) already exists"
+      else
+        puts "+++ seeding INTEGER #{t.id} (as used by #{t.user.name} on Twitter)"
+        url = "http://twitter.com/#{t.user.screen_name}/status/#{t.id}"
+        # note: don't use #create since id is attr_protected
+        csi = CsInteger.new( 
+          :celebrity_name => t.user.name,
+          :celebrity_screen_name => t.user.screen_name,
+          :origin => 'Twitter',
+          :origin_url => url
+        )
+        csi.id = t.id #likewise, need to set id seperately here
+        csi.save!
+      end
     end
+  rescue Twitter::Error::Unauthorized
+    puts "Got Unauthorized error from twitter trying to read #{celeb}"
   end
 end
