3369b335a1e2de480b43cac6ebd3679442caaa50
Matthew Rothenberg <mrothenberg@gmail.com>
2012-08-17 18:52:02 -0400

    dont try to reinsert records on seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -8,17 +8,21 @@
 
 File.open('config/twitter_celebs.txt').each do |celeb|
   Twitter.user_timeline( celeb ).each do |t|
-    puts "seeding INTEGER #{t.id} (as used by #{t.user.name} on Twitter)"
-    url = "http://twitter.com/#{t.user.screen_name}/status/#{t.id}"
 
-    # note: don't use #create since id is attr_protected
-    csi = CsInteger.new( 
-      :celebrity_name => t.user.name,
-      :celebrity_screen_name => t.user.screen_name,
-      :origin => 'Twitter',
-      :origin_url => url
-    )
-    csi.id = t.id #likewise, need to set id seperately here
-    csi.save!
+    if CsInteger.exists? t.id
+      puts "--- INTEGER #{t.id} (as used by #{t.user.name}) already exists"
+    else
+      puts "+++ seeding INTEGER #{t.id} (as used by #{t.user.name} on Twitter)"
+      url = "http://twitter.com/#{t.user.screen_name}/status/#{t.id}"
+      # note: don't use #create since id is attr_protected
+      csi = CsInteger.new( 
+        :celebrity_name => t.user.name,
+        :celebrity_screen_name => t.user.screen_name,
+        :origin => 'Twitter',
+        :origin_url => url
+      )
+      csi.id = t.id #likewise, need to set id seperately here
+      csi.save!
+    end
   end
 end
