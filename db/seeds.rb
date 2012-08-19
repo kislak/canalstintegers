e2df2d2f107adc7dd778913a3f9b100df5ef9abc
Matthew Rothenberg <mrothenberg@gmail.com>
2012-08-19 16:26:46 -0400

    use new method to seed from tweet

--- db/seeds.rb
+++ db/seeds.rb
@@ -9,21 +9,11 @@
 File.open('config/twitter_celebs.txt').each do |celeb|
   begin
     Twitter.user_timeline( celeb ).each do |t|
-
-      if CsInteger.exists? t.id
+      begin
+        CsInteger.create_from_tweet(t)
+        puts "+++ seeded INTEGER #{t.id} (as used by #{t.user.name} on Twitter)"
+      rescue CsIntegerAlreadyExistsError
         puts "--- INTEGER #{t.id} (as used by #{t.user.name}) already exists"
-      else
-        puts "+++ seeding INTEGER #{t.id} (as used by #{t.user.name} on Twitter)"
-        url = "http://twitter.com/#{t.user.screen_name}/status/#{t.id}"
-        # note: don't use #create since id is attr_protected
-        csi = CsInteger.new( 
-          :celebrity_name => t.user.name,
-          :celebrity_screen_name => t.user.screen_name,
-          :origin => 'Twitter',
-          :origin_url => url
-        )
-        csi.id = t.id #likewise, need to set id seperately here
-        csi.save!
       end
     end
   rescue Twitter::Error::Unauthorized
